1461a9419d7abefa9f5e7a5dd25db52f6ddbf4c6
Avram86 <60349668+Avram86@users.noreply.github.com>
2021-08-31 11:19:53 +0300

    Basic roles seeded

--- GestionarePacienti/Data/Migrations/20210831074950_SeedRoles.Designer.cs
+++ GestionarePacienti/Data/Migrations/20210831074950_SeedRoles.Designer.cs
@@ -4,14 +4,16 @@ using GestionarePacienti.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace GestionarePacienti.Migrations
 {
     [DbContext(typeof(GestionarePacientiContext))]
-    partial class GestionarePacientiContextModelSnapshot : ModelSnapshot
+    [Migration("20210831074950_SeedRoles")]
+    partial class SeedRoles
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ GestionarePacienti/Data/Migrations/20210831074950_SeedRoles.cs
@@ -0,0 +1,28 @@
+﻿using GestionarePacienti.Data.Extensions;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GestionarePacienti.Migrations
+{
+    public partial class SeedRoles : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddRole("admin");
+            migrationBuilder.AddRole("operator");
+
+            migrationBuilder.AddUserWithRoles(
+                "admin@accountsapp.com",
+                "Admin123!",
+                new[] { "admin"});
+
+            migrationBuilder.AddUserWithRoles(
+                "operator@accountsapp.com",
+                "Admin123!",
+                new[] { "operator" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+        }
+    }
+}
